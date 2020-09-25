b2ec3eaf0b07035cdbfcf87ba43a571bc23a770a
姜玉玺 <jiangyx@hd100.com>
2020-09-25 16:40:00 +0800

    启动停止脚本

--- jenkins/src/main/cmd/start.bat
+++ jenkins/src/main/cmd/start.bat
@@ -1,7 +1,2 @@
-@echo off 　　
-if "%1" == "h" goto begin
- 　mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
-:begin
-
 cd ../../../target/
 java -jar jenkins-0.0.1-SNAPSHOT.jar
